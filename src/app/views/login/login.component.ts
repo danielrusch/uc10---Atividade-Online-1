d7ea4f1fca2909266df72a5be57ada103718ecc7
Daniel Rusch <daniel-rusch@hotmail.com>
2022-10-26 22:55:45 -0300

    mensagens login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +10,22 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private LoginService:LoginService) { }
+  constructor(private LoginService:LoginService,private router: Router ) { }
 
   ngOnInit(): void {
   }
 
   userModel = new User();
+
+  mensagem = ""
   
   receberDados() {this.LoginService.login (this.userModel).subscribe((response) => {
-    console.log(response)
+    console.log("deu certo")
+    this.router.navigateByUrl("/")
+  }, (erro) => {
+    // console.log(erro)
+    // alert(erro.error)
+    this.mensagem = erro.error
   })
   
   }
